36701ba1498a2debf133d8805ae837c15b1e30c5
DimaChere <dmitriy.cherepanov02@mail.ru>
2024-09-17 19:59:18 +0600

    feat: react-hook-form login

--- src/components/LoginPopUp/LoginPopUp.tsx
+++ src/components/LoginPopUp/LoginPopUp.tsx
@@ -1,43 +1,41 @@
-import { useState } from "react";
 import "./LoginPopUp.style.sass";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../../app/store/features/boardSlice";
 import { RootState } from "../../app/store/store";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface LoginForm {
+    userName: string;
+}
 
 export const LoginPopUp: React.FC = () => {
-    const [userName, setUserName] = useState<string>("");
-    const [isRegisterSuccess, setIsRegisterSuccess] = useState<boolean>(false);
     const dispatch = useDispatch();
     const user = useSelector((state: RootState) => state.board.user);
+    const { register, handleSubmit } = useForm<LoginForm>();
+    const onSubmit: SubmitHandler<LoginForm> = (data) =>
+        dispatch(addUser({ userName: data.userName }));
 
     if (user) {
         return null;
     }
 
-    const handleUserRegister = () => {
-        dispatch(addUser({ userName }));
-        setIsRegisterSuccess(true);
-        setUserName("");
-    };
-
-    if (isRegisterSuccess) {
-        return null;
-    }
-
     return (
         <div className="pop-up-background">
-            <div className="pop-up">
+            <form onSubmit={handleSubmit(onSubmit)} className="pop-up">
                 <label className="pop-up__title">Пользователь:</label>
                 <input
-                    type="text"
+                    {...register("userName", {
+                        required: true,
+                        maxLength: 20,
+                    })}
                     className="pop-up__input"
-                    value={userName}
-                    onChange={(e) => setUserName(e.target.value)}
                 />
-                <button className="pop-up__button" onClick={handleUserRegister}>
-                    Зарегестрировать пользователя
-                </button>
-            </div>
+                <input
+                    type="submit"
+                    className="pop-up__button"
+                    value="Зарегистрировать пользователя"
+                />
+            </form>
         </div>
     );
 };
